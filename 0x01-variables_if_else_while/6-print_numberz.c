f915ccadd26e01034c65ad4656f96e8c5a6c0d90
Brightside2456 <sefahb13@gmail.com>
2023-02-26 15:40:25 -0800

    Print numbers from 1 t0 10 but with thier ASCII codes instead and using the putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 30; num <= 39;)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+
+	return (0);
+}
