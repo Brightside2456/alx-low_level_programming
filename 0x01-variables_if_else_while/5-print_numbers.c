cbae9ea260f2b64ab1bbce95d62ea46d775d79a8
Brightside2456 <sefahb13@gmail.com>
2023-02-26 15:25:52 -0800

    Print numbers from 1 t0 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; )
+	{
+		printf("%d", num);
+		num++;
+	}
+	return (0);
+}
