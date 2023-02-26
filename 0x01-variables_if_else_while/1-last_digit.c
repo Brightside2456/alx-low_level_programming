f01eee057bb234602b77266065d4ba152de8cd81
Brightside2456 <sefahb13@gmail.com>
2023-02-26 09:40:34 -0800

    Checking whether a number is +, _ or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,23 +11,24 @@
 int main(void)
 {
 	int n;
+	
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	int ans = n % 10;
-	printf("Last digit of %d is %d ", n, ans);
+
 	if (ans > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, ans);
 	}
 	else if (ans == 0)
 	{
-		printf("and is 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, ans);
 	}
 	else if (ans < 6 && ans != 0)
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ans);
 	}
 
 	return (0);
