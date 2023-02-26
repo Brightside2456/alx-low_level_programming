b62bf93963fbc2f52b90608e6aeda044d3d5f61b
Brightside2456 <sefahb13@gmail.com>
2023-02-26 06:59:40 -0800

    Using if conditions together with conditional operators

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -15,5 +15,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	int ans = n % 10;
+	printf("Last digit of %d is %d ", n, ans);
+	if (ans > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (ans == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (ans < 6 && ans != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
 	return (0);
 }
