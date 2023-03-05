97ca005bb2b894223a2910dd54d3366ea5215cf3
Brightside2456 <sefahb13@gmail.com>
2023-03-05 14:02:31 -0800

    a program to print the sum of all multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no params
+ *
+ * Return: integer always
+ */
+int main(void)
+{
+	int total;
+	int i;
+
+	total = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			total += i;
+		}
+		else
+		{
+
+		}
+	}
+	printf("%d", total);
+	printf("\n");
+}
