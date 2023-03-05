39dffefcddf829098284c245bbe19c926494e10e
Brightside2456 <sefahb13@gmail.com>
2023-03-05 03:44:16 -0800

    a func to print a last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints out the last digit
+ *
+ * @a: the digit to be worded on
+ *
+ * Return: an int on success on faliure
+ */
+
+int print_last_digit(int a)
+{
+	int value;
+
+	value = a % 10;
+
+	return (value);
+}
