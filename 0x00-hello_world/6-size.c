9192f4f0ed2edb1b232d8dd71d4bb4129191b0ac
Bright Sefah <sefahb13@gmail.com>
2023-05-19 05:45:20 +0000

    Printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	int a = 'a';
-	int b = 1;
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float e;
 
-	printf("Size of a char: %ld byte(s)", sizeof(a));
-	printf("Size of a int: %ld byte(s)", sizeof(b));
-	printf("Size of a long int: %ld byte(s)", sizeof(long unsigned int));
-	printf("Size of a long long: %ld byte(s)", sizeof(long unsigned int));
-	printf("Size of a float: %ld byte(s)", sizeof(long unsigned int));
+	printf("Size of a char: %c byte(s)", sizeof(a));
+	printf("Size of a int: %d byte(s)", sizeof(b));
+	printf("Size of a long int: %ld byte(s)", sizeof(c));
+	printf("Size of a long long: %lld byte(s)", sizeof(d));
+	printf("Size of a float: %f byte(s)", sizeof(e));
 
 	return (0);
 }
