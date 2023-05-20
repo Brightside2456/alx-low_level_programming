a34aac91226e5a0c310af69fcef9c31a3b545761
Bright Sefah <sefahb13@gmail.com>
2023-05-20 03:19:01 +0000

    Print all combinations of single digit nums

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	/* your code goes there */
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+
+	return (0);
+}
