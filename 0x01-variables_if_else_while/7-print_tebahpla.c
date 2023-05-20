7176e6ca2c9d24bee01535f4ac0374d2e1e06079
Bright Sefah <sefahb13@gmail.com>
2023-05-20 02:53:11 +0000

    Reverse alphabets using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char n;
+
+	/* your code goes there */
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar(10);
+
+	return (0);
+}
