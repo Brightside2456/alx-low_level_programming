e628495383ac597c36cd7b7e76a7849c184f548e
Bright Sefah <sefahb13@gmail.com>
2023-05-30 23:00:44 +0000

    103-fibb

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	long int num1 = 1;
 	long int num2 = 2;
-	int i;
 	long int total;
+	long int sum;
 
-	for (i = 1; i <= 48; i++)
+	while (sum <= 4000000)
 	{
 		if (num2 % 2 == 0)
 		{
-			long int sum = num1 + num2;
-
-			num1 = num2;
-			num2 = sum;
+			total += num2;
 		}
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
 	}
 	printf("%ld\n", total);
 	return (0);
