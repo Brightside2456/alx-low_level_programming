3b55cf0e1b1a6aace7d505a8172467d50e866056
Bright Sefah <sefahb13@gmail.com>
2023-05-30 23:59:09 +0000

    print times table up to n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,10 @@
  */
 void print_times_table(int n)
 {
+	int num1, num2;
+
 	if (n >= 0 && n <= 15)
 	{
-		int num1, num2;
-
 		for (num1 = 0; num1 <= n; num1++)
 		{
 			for (num2 = 0; num2 <= n; num2++)
@@ -19,36 +19,29 @@ void print_times_table(int n)
 				int product = num1 * num2;
 
 				if (num2 == 0)
-				{
 					_putchar('0' + product);
-				}
-				else if (product < 10)
+				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					if (product < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + (product / 10) % 10);
+					}
 					_putchar('0' + (product % 10));
-				}
-				else if (product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (product / 100));
-					_putchar('0' + (product / 10) % 10);
-					_putchar('0' + (product % 10));
-				}
-				if (num2 == n)
-				{
-					_putchar('\n');
+					if (num2 == n)
+						_putchar('\n');
 				}
 			}
 		}
--- 0x02-functions_nested_loops/test1.c
+++ 0x02-functions_nested_loops/test1.c
@@ -1,30 +1,43 @@
 #include "main.h"
 
+/**
+ * print_times_table - print times table up to n
+ *
+ * @n: parameter
+ *
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
-	int num1;
-	int num2;
-
-	for (num1 = 0; num1 <= n; num1++)
+	if (n >= 0 && n <= 15)
 	{
-		for (num2 = 0; num2 <= n; num2++)
+		for (int num1 = 0; num1 <= n; num1++)
 		{
-			int product = num1 * num2;
-
-			if (num2 > 0)
+			for (int num2 = 0; num2 <= n; num2++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				int product = num1 * num2;
+				
+				if (num2 == 0)
+				{
+					_putchar('0' + product);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
+					if (product < 100)
+						_putchar(' ');
+					_putchar('0' + (product / 100) % 10);
+					_putchar('0' + (product / 10) % 10);
+					_putchar('0' + (product % 10));
+				}
+				if (num2 == n)
+				{
+					_putchar('\n');
+				}
 			}
-			if (product < 10)
-				_putchar(' ');
-			else if (product < 100)
-				_putchar('0' + (product / 10));
-			_putchar('0' + (product % 10));
-			if (num2 == n)
-				_putchar('\n');
 		}
 	}
 }
