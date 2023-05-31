42fb4e73f1a0da1a63370ce253a81f8244924b0d
Bright Sefah <sefahb13@gmail.com>
2023-05-31 00:39:04 +0000

    End of Task 0x02

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	long int num1 = 1;
+	long int num2 = 2;
+	long int m, i;
+	
+	printf("%lu, %lu", num1, num2);
+	for (i = 1; i <= 96; i++)
+	{
+		m = num1 + num2;
+		num1 = num2;
+		num2 = m;
+		printf(", %lu", num2);
+	}
+	putchar('\n');
+	return (0);
+}
