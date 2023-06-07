3ed4292640f4a2c08cf06133d17f2d0f6538ba1b
Bright Sefah <sefahb13@gmail.com>
2023-06-07 11:36:03 +0000

    Testing header files and funtions

---
+++ 0x03-debugging/test2.c
@@ -0,0 +1,14 @@
+#include "bright.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
