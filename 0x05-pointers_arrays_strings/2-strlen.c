affb57b1f3bcacfb94263510d6520819076f4b78
Bright Sefah <sefahb13@gmail.com>
2023-06-11 22:21:19 +0000

    Count the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _str_len - check the lenght of a string
+ *
+ * @s: The string parameter
+ *
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
