971a644f0bf928aa43cf8a3400680a327e410b1c
Bright Sefah <sefahb13@gmail.com>
2023-06-13 02:32:16 +0000

    print n members in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_array - Funtion to print array *a of lenght n
+ * @a: array
+ * @n: number to print
+ * Return: null
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", a[0]);
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", a[i]);
+	}
+	putchar('\n');
+}
