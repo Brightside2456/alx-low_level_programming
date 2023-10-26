a4c99d332d553b10246bc37bca2e34bf12731c6d
Bright Sefah <sefahb13@gmail.com>
2023-10-26 04:53:14 +0000

    compleated task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,6 +7,16 @@
  * Return: value of x raised to y
  */
 
-int _poe_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	/* Recursive Step: x^y = x* x^(y-1) */
+	return (x * _pow_recursion(x, y - 1));
 }
