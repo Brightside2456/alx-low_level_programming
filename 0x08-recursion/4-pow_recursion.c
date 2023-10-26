ef7da8a8c9ac9d0bd69813e6044c87d9587bbc7d
Bright Sefah <sefahb13@gmail.com>
2023-10-26 04:38:00 +0000

    Add readme file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate value of x raised to y
+ * @x: base number
+ * @y: index
+ * Return: value of x raised to y
+ */
+
+int _poe_recursion(int x, int y)
+{
+}
