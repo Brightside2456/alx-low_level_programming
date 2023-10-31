722c31b686996d6fb4281471285bb46cebf32f95
Bright Sefah <sefahb13@gmail.com>
2023-10-31 16:39:01 +0000

    Ctreate function to return a pointer to a newly allocated space in memory which is a duplicate of the parameter string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to an new but duplicate str
+ * @str: string to be duplicated
+ * Return: pointer to new but 'copied' string
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i;
+	int j;
+
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	array = malloc(sizeof(char) * (i + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		array[j] = str[j];
+	}
+	return (array);
+}
