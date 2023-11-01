080dc7d627255196f71a859e09b6d6d7b8846ede
Bright Sefah <sefahb13@gmail.com>
2023-11-01 04:42:27 +0000

    task 4 free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - function to  deallocate the allocated space for int grid
+ * @grid: pointer to int *
+ * @height: height of grid
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+
+}
