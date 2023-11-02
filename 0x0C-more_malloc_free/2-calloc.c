d823494ef772c6520587fddca5af265920db3e35
Bright Sefah <sefahb13@gmail.com>
2023-11-02 16:07:35 +0000

    Create function to allocate memory for array of n members of size 'size' bytes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *array;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -16,6 +17,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (array == NULL)
 		return (NULL);
-	
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		*array[i] = 0;
+	}
+
 	return (array);
 }
