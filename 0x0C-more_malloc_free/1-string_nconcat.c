664fd3c9b965f188d4ea7f60e2031fd56c40353b
Bright Sefah <sefahb13@gmail.com>
2023-11-02 17:10:47 +0000

    BugFix in task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,50 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * string_nconcat - concatenate to  the nth value of s2
- * @s1: pointer to hold string1
- * @s2: pointer to hold string2
- * @n: int
- * Return: string
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i, j, k, total_length;
-
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 ="";
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-
-	if (n >= j)
-		n = j;
-	total_length = i + n;
-	ptr = malloc(sizeof(char) *  (total_length + 1));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (k = 0; k < total_length; k++)
 	{
-		k < i ? (ptr[k] = s1[k]) : (ptr[k] = s2[k - i]);
+		s1 = "";
 	}
-	ptr[total_length] = '\0';
-	return (ptr);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+	while (s2[size2] != '\0')
+	{
+	size2++;
+	}
+	if (n > size2)
+	{
+		n = size2;
+	}
+	p = malloc((size1 + n + 1) * sizeof(char));
+	if (p == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+	return (p);
 }
