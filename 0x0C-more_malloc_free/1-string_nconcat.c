a13d353f262db096c21ef2ace5b8b8bd2c20d3d1
Bright Sefah <sefahb13@gmail.com>
2023-11-02 14:51:56 +0000

    string to copy the entire first param and first n bytes of second param

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 		n = j;
 	total_length = i + n;
-	ptr = malloc(sizeof(char) *  total_length);
+	ptr = malloc(sizeof(char) *  (total_length + 1));
 
 	if (ptr == NULL)
 		return (NULL);
