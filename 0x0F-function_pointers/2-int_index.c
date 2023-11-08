6fc22f0303e9bc048f72645f7b6a33e033764dde
Bright Sefah <sefahb13@gmail.com>
2023-11-08 22:51:47 +0000

    Return index of first array alement that passed the test

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - returns the index of the first array element that meets
+ * the requirement
+ * @array: Array
+ * @size: number of array elements
+ * @cmp: pointer to function
+ * Return: index of specified array element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int (*f)(int);
+
+	f = cmp;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (f(*(array + i)))
+			return (i);
+	return (-1);
+}
+
