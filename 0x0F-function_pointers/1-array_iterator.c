a27c92c5e2c638b1663e95c0dfa6c292b2e97de5
Bright Sefah <sefahb13@gmail.com>
2023-11-08 22:31:29 +0000

    Iterate over array elements and perform same func on them

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator -  execute function on every element in array
+ * @array: array element
+ * @size: size of array
+ * @action: function pointer
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	void (*act)(int);
+
+	act = action;
+	for (i = 0; i < size; i++, *array++)
+		act(*array);
+}
