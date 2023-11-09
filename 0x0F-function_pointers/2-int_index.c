bda2316d92807082a5fef1fb9ec3aa15f42703fc
Bright Sefah <sefahb13@gmail.com>
2023-11-09 00:52:42 +0000

    BugFix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,31 +1,27 @@
+ 
+ 
+;
+#include <stdlib.h>
 #include "function_pointers.h"
-
 /**
-  * int_index - ...
-  * @array: ...
-  * @size: ...
-  * @cmp: ...
-  *
-  * Return: ...
-  */
+ * int_index - returns the index of the first array element that meets
+ * the requirement
+ * @array: Array
+ * @size: number of array elements
+ * @cmp: pointer to function
+ * Return: index of specified array element
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
+	int (*f)(int);
 
-	if (size > 0)
-	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-				{
-					return (i);
-				}
-				i++;
-			}
-		}
-	}
-
+	f = cmp;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (f(*(array + i)))
+			return (i);
 	return (-1);
 }
+
