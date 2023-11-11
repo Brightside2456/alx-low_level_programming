169b8c7ee325be406fee54b2d2f560f1d9685d0a
Bright Sefah <sefahb13@gmail.com>
2023-11-11 15:33:00 +0000

    print all n strings with separator as delimiter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints n number of strings
+ * @separator: a delimeter for the insividual sttrings
+ * @n: the number of strings to print
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+	va_list strings;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(strings, char *);
+		if (string == NULL)
+			printf("nil");
+		else
+		{
+			printf("%s", string);
+		}
+		if ((i + 1) == n)
+			break;
+		printf("%s", separator);
+	}
+	printf("\n");
+}
