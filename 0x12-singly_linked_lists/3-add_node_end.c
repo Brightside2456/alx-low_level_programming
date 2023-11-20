f0b46dafbdd6fd516eb005a0e2923e51e7d730b3
Bright Sefah <sefahb13@gmail.com>
2023-11-20 10:56:42 +0000

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,7 +5,7 @@
  * add_node_end - add a node to the end of another node
  * @head: pointer that points to the head of the list node
  * @str: string value
- * Return: the address of the new element or NULL if failed 
+ * Return: the address of the new element or NULL if failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
