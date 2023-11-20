27670912a828a96012aa3399640a7c5b7a4086b8
Bright Sefah <sefahb13@gmail.com>
2023-11-20 11:29:51 +0000

    free nodes in type list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - free a list_t type list
+ * @head: the head pointer of a list
+ * @Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
